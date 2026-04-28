70885e31af8ebcee641e2a7c354b60a1934818ec
Okamoto <85035999+Okamoto148@users.noreply.github.com>
2026-04-27 23:00:18 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // Construtor recebendo todos os parâmetros
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -21,6 +26,7 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato que será sobrescrito nas classes filhas
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
